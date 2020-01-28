b443ac0fb6bd6c9bbdc2899512cb0c2d13d6d8ea
Léonard <epoc4oftime@gmail.com>
2020-01-28 21:29:13 +0100

    TW_Recognize whether element is compatible

--- SemaineIntensive_FlexCognitive/Assets/Scripts/LeoScripts/SortingBoxBehavior.cs
+++ SemaineIntensive_FlexCognitive/Assets/Scripts/LeoScripts/SortingBoxBehavior.cs
@@ -32,22 +32,26 @@ public class SortingBoxBehavior : MonoBehaviour
         {
             case State.Color:
                 if (collisionObject.name.Contains("Green")) Debug.Log("I've hit Green");
-                if (collisionObject.name.Contains("Blue")) Debug.Log("I've hit Blue");
-                if (collisionObject.name.Contains("Red")) Debug.Log("I've hit Red");
+                else if (collisionObject.name.Contains("Blue")) Debug.Log("I've hit Blue");
+                else if (collisionObject.name.Contains("Red")) Debug.Log("I've hit Red");
+                else Debug.Log("Incompatible");
                 break;
             case State.Shape:
                 if (collisionObject.name.Contains("Square")) Debug.Log("I've hit Square");
-                if (collisionObject.name.Contains("Circle")) Debug.Log("I've hit Circle");
+                else if (collisionObject.name.Contains("Circle")) Debug.Log("I've hit Circle");
+                else Debug.Log("Incompatible");
                 break;
             case State.Number:
                 if (collisionObject.name.Contains("One")) Debug.Log("I've hit 1");
-                if (collisionObject.name.Contains("Two")) Debug.Log("I've hit 2");
-                if (collisionObject.name.Contains("Three")) Debug.Log("I've hit 3");
+                else if (collisionObject.name.Contains("Two")) Debug.Log("I've hit 2");
+                else if (collisionObject.name.Contains("Three")) Debug.Log("I've hit 3");
+                else Debug.Log("Incompatible");
                 break;
             case State.Letter:
                 if (collisionObject.name.Contains("Aee")) Debug.Log("I've hit A");
-                if (collisionObject.name.Contains("Bee")) Debug.Log("I've hit B");
-                if (collisionObject.name.Contains("Cee")) Debug.Log("I've hit C");
+                else if (collisionObject.name.Contains("Bee")) Debug.Log("I've hit B");
+                else if (collisionObject.name.Contains("Cee")) Debug.Log("I've hit C");
+                else Debug.Log("Incompatible");
                 break;
         }
     }
