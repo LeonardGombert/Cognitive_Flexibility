2c632b1901936c9599aa37dcd50a11d653923dda
Léonard <epoc4oftime@gmail.com>
2020-01-28 16:36:34 +0100

    FT_Basic  Functions and Components (1)
    Mouse behavior, Object Spawning, Object class

---
+++ SemaineIntensive_FlexCognitive/Assets/Scripts/LeoScripts/MouseBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseClick();
+    }
+
+    private void MouseClick()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider.tag == "Object")
+            {
+                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+            }
+
+            if (hit.collider == null) print("nothing here");
+        }
+    }
+}
--- SemaineIntensive_FlexCognitive/Assets/Scripts/LeoScripts/ObjectGenerator.cs
+++ SemaineIntensive_FlexCognitive/Assets/Scripts/LeoScripts/ObjectGenerator.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] GameObject objectPrefab;
+    [SerializeField] int numberToSpawn;
+    int onStartSpawn;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SpawnObjects();
+    }
+
+    private void SpawnObjects()
+    {
+        for (onStartSpawn = 0; onStartSpawn < numberToSpawn; onStartSpawn++)
+        {
+            Instantiate(objectPrefab, gameObject.transform);
+        }
     }
 }
