3609f5be3acd6cd31f40c083307d1b758b9552c2
Léonard <epoc4oftime@gmail.com>
2020-01-29 11:31:00 +0100

    CL_Sentence Generation Variables

---
+++ SemaineIntensive_FlexCognitive/Assets/Scripts/LeoScripts/SentenceGenerator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class SentenceGenerator : MonoBehaviour
+{
+    //SENTENCE VARIABLES
+    string action, type, color, sentence1, sentence2;
+
+    string[] actionLines;
+    string[] typeLines;
+    string[] colorLines;
+    [SerializeField] TextAsset actionText;
+    [SerializeField] TextAsset typeText;
+    [SerializeField] TextAsset colorText;
+
+    public enum currentAction { trier, nePasTrier, sortir, nePasSortir};
+
+    [ShowInInspector] public static currentAction firstSentence;
+    [ShowInInspector] public static currentAction secondSentence;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateSentence();
+    }
+
+    private void GenerateSentence()
+    {
+        if (actionText != null && typeText != null && colorText != null)
+        {
+            actionLines = (actionText.text.Split('\n'));
+            typeLines = (typeText.text.Split('\n'));
+            colorLines = (colorText.text.Split('\n'));
+
+            ActionTypeCheck(1);
+
+            type = typeLines[UnityEngine.Random.Range(0, 4)];
+            color = colorLines[UnityEngine.Random.Range(0, 3)];
+            sentence1 = string.Format("{0} {1} {2}.", action, type, color);
+
+            ActionTypeCheck(2);
+
+            type = typeLines[UnityEngine.Random.Range(0, 4)];
+            color = colorLines[UnityEngine.Random.Range(0, 3)];
+            sentence2 = string.Format("{0} {1} {2}.", action, type, color);
+
+            Debug.Log(sentence1);
+            Debug.Log(sentence2);
+
+            SentenceToGeneration(sentence1);
+            SentenceToGeneration(sentence2);
+            //GetComponent<TextMesh>().text = dialog;
+        }
+    }
+
+    private void SentenceToGeneration(string sentenceToRead)
+    {
+
+    }
+
+    private void ActionTypeCheck(int whichSentence)
+    {
+        if(whichSentence == 1)
+        {
+            switch (firstSentence)
+            {
+                case currentAction.trier:
+                    action = actionLines[0];
+                    break;
+                case currentAction.nePasTrier:
+                    action = actionLines[1];
+                    break;
+                case currentAction.sortir:
+                    action = actionLines[2];
+                    break;
+                case currentAction.nePasSortir:
+                    action = actionLines[3];
+                    break;
+            }
+        }
+
+        if (whichSentence == 2)
+        {
+            switch (secondSentence)
+            {
+                case currentAction.trier:
+                    action = actionLines[0];
+                    break;
+                case currentAction.nePasTrier:
+                    action = actionLines[1];
+                    break;
+                case currentAction.sortir:
+                    action = actionLines[2];
+                    break;
+                case currentAction.nePasSortir:
+                    action = actionLines[3];
+                    break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+
+    }
+}
